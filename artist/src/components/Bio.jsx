7b70748c49990fb95c6ff2860e9fa503f4428964
Semir Sultan Mohammed <semi512113@gmail.com>
2025-02-15 19:27:22 +0300

    added pc and tablet designs, mostly finalized

--- artist/src/components/Bio.jsx
+++ artist/src/components/Bio.jsx
@@ -5,7 +5,7 @@ export default function Bio(){
         <div className="about">
             <div className="title">
                 <img src="	https://tewoldebrehan.art/wp-content/uploads/2017/11/artist2-home-icon.png" alt="photo" />
-                <p>About Me</p>
+                <p>ABOUT ME</p>
             </div>
             <div className="bio">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae tenetur laudantium eligendi libero quod qui, corrupti error consequatur aspernatur suscipit!</p>
